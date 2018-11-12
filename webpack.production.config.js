c78529519144291c05627b3ac7be6ef6f7445654
shufengwu5946 <594695686@qq.com>
2018-11-13 01:46:16 +0800

    基本使用

--- webpack.production.config.js
+++ webpack.production.config.js
@@ -3,13 +3,14 @@ const HtmlWebpackPlugin = require('html-webpack-plugin');
 // extract-text-webpack-plugin暂时不支持webpack4
 // const ExtractTextPlugin = require('extract-text-webpack-plugin');
 const UglifyjsWebpackPlugin = require('uglifyjs-webpack-plugin');
+const CleanWebpackPlugin = require("clean-webpack-plugin");
 
 module.exports = {
     devtool: 'null', //注意修改了这里，这能大大压缩我们的打包代码
     entry: __dirname + "/app/index.js", //已多次提及的唯一入口文件
     output: {
         path: __dirname + "/dist", //打包后的文件存放的地方
-        filename: "index.js" //打包后输出文件的文件名
+        filename: "index-[hash].js" //打包后输出文件的文件名
     },
 
     devServer: {
@@ -26,27 +27,30 @@ module.exports = {
             exclude: /node_modules/
         }, {
             test: /\.css$/,
-            use: [
-                {
-                    loader: "style-loader"
-                }, {
-                    loader: "css-loader",
-                    options: {
-                        modules: true, // 指定启用css modules
-                        localIdentName: '[name]__[local]--[hash:base64:5]' // 指定css的类名格式
-                    }
+            use: [{
+                loader: "style-loader"
+            }, {
+                loader: "css-loader",
+                options: {
+                    modules: true, // 指定启用css modules
+                    localIdentName: '[name]__[local]--[hash:base64:5]' // 指定css的类名格式
                 }
-            ]
+            }]
         }]
     },
     plugins: [
         new webpack.BannerPlugin('版权所有，翻版必究'),
         new HtmlWebpackPlugin({
-            template: __dirname + "/app/index.tmpl.html"//new 一个这个插件的实例，并传入相关的参数
+            template: __dirname + "/app/index.tmpl.html" //new 一个这个插件的实例，并传入相关的参数
         }),
         new webpack.optimize.OccurrenceOrderPlugin(),
         new UglifyjsWebpackPlugin(),
         // extract-text-webpack-plugin暂时不支持webpack4
         // new ExtractTextPlugin("style.css"),
+        new CleanWebpackPlugin('dist/*.*', {
+            root: __dirname,
+            verbose: true,
+            dry: false
+        })
     ],
 }
